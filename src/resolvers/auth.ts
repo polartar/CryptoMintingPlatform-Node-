e66f0a06b66ce404a83cd142fa4df34708baba7d
polartar <polaris.center@outlook.com>
2019-06-19 13:55:23 -0600

    feat: 2fa register and validate

--- src/resolvers/auth.ts
+++ src/resolvers/auth.ts
@@ -1,6 +1,7 @@
 import { auth } from '../common';
 import { Context } from '../types/context';
 import ResolverBase from '../common/Resolver-Base';
+import { ApolloError } from 'apollo-server-express';
 const autoBind = require('auto-bind');
 class Resolvers extends ResolverBase {
   constructor() {
@@ -16,13 +17,37 @@ class Resolvers extends ResolverBase {
     const token = await auth.signIn(args.token, domain);
     return { token };
   }
+
+  public async twoFaRegister(parent: any, args: {}, { user }: Context) {
+    this.requireAuth(user);
+    const { twoFaSecret } = await user.findFromDb();
+    if (twoFaSecret) {
+      throw new ApolloError('Two Factor Authentication is already set up.');
+    }
+    const qrCode = await user.setTempTwoFaSecret();
+
+    return { qrCode };
+  }
+
+  public async twoFaValidate(
+    parent: any,
+    args: { totpToken: string },
+    { user }: Context,
+  ) {
+    this.requireAuth(user);
+    const authenticated = await user.validateTwoFa(args.totpToken);
+    return { authenticated };
+  }
 }
 
 const resolvers = new Resolvers();
 
 export default {
-  Query: {},
+  Query: {
+    twoFaValidate: resolvers.twoFaValidate,
+  },
   Mutation: {
     login: resolvers.login,
+    twoFaRegister: resolvers.twoFaRegister,
   },
 };
