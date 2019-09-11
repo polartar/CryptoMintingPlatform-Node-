a341ee2ea6e2362e98147226ee2e74d601e7e974
polartar <polaris.center@outlook.com>
2019-09-11 14:07:54 -0600

    feat: shareUser query

--- src/resolvers/share.ts
+++ src/resolvers/share.ts
@@ -1,39 +1,85 @@
 import ResolverBase from '../common/Resolver-Base';
 import { Context } from '../types/context';
-import { config } from '../common';
+import { config, logger } from '../common';
 import { IWalletEnvironment } from '../models/environment';
+import autoBind = require('auto-bind');
 
 class Resolvers extends ResolverBase {
+  constructor() {
+    super();
+    autoBind(this);
+  }
+
   public async shareConfig(
     parent: any,
     args: {},
-    { dataSources: { environment } }: Context,
+    { dataSources: { environment }, user }: Context,
   ) {
-    const {
-      walletCompanyFee: companyFee,
-      walletReferrerReward: referrerReward,
-      walletRewardAmount: rewardAmount,
-      walletRewardCurrency: rewardCurrency,
-      walletShareLimit: shareLimit,
-      walletUserBalanceThreshold: userBalanceThreshold,
-    } = (await environment.findOne({
-      domain: config.hostname,
-    })) as IWalletEnvironment;
-    return {
-      referrerReward,
-      companyFee,
-      rewardCurrency,
-      rewardAmount,
-      userBalanceThreshold,
-      shareLimit,
-    };
+    logger.debug(
+      `resolvers.share.shareConfig.user.userId: ${user && user.userId}`,
+    );
+    this.requireAuth(user);
+    try {
+      const {
+        walletCompanyFee: companyFee,
+        walletReferrerReward: referrerReward,
+        walletRewardAmount: rewardAmount,
+        walletRewardCurrency: rewardCurrency,
+        walletShareLimit: shareLimit,
+        walletUserBalanceThreshold: userBalanceThreshold,
+      } = (await environment.findOne({
+        domain: config.hostname,
+      })) as IWalletEnvironment;
+      const shareConfigResponse = {
+        referrerReward,
+        companyFee,
+        rewardCurrency,
+        rewardAmount,
+        userBalanceThreshold,
+        shareLimit,
+      };
+      logger.debug(
+        `resolvers.share.shareConfig.shareConfigResponse: ${shareConfigResponse}`,
+      );
+      return shareConfigResponse;
+    } catch (error) {
+      logger.warn(`resolvers.share.shareConfig.catch: ${error}`);
+      throw error;
+    }
   }
-  // public async shareUser(
-  //   parent: any,
-  //   args: {},
-  //   context: Context
-  // ) {
-  // }
+  public async shareUser(parent: any, args: {}, { user, wallet }: Context) {
+    logger.debug(
+      `resolvers.share.shareUser.user.userId: ${user && user.userId}`,
+    );
+
+    this.requireAuth(user);
+    try {
+      const { wallet: userWallet } = await user.findFromDb();
+      if (!userWallet) throw new Error('User wallet not initialized');
+      logger.debug(
+        `resolvers.share.shareUser.userWallet.activated: ${
+          userWallet.activated
+        }`,
+      );
+      if (!userWallet.activated) {
+        return { activated: false };
+      }
+      const { confirmed, unconfirmed } = await wallet
+        .coin('btc')
+        .getBalance(user.userId);
+      const shareUserResponse = {
+        activated: true,
+        btcBalanceConfirmed: confirmed,
+        btcBalancePending: unconfirmed,
+      };
+      logger.debug(
+        `resolvers.share.shareUser.shareUserResponse: ${shareUserResponse}`,
+      );
+    } catch (error) {
+      logger.warn(`resolvers.share.shareUser.catch: ${error}`);
+      throw error;
+    }
+  }
 }
 
 const resolvers = new Resolvers();
@@ -41,6 +87,6 @@ const resolvers = new Resolvers();
 export default {
   Query: {
     shareConfig: resolvers.shareConfig,
-    // shareUser: resolvers.shareUser,
+    shareUser: resolvers.shareUser,
   },
 };
