a3a1a5dce03df2eb4364ce857a38758857dc5d7f
polartar <polaris.center@outlook.com>
2019-11-01 10:09:36 -0600

    Emails not quite ready for prime time

--- src/services/email.ts
+++ src/services/email.ts
@@ -5,64 +5,61 @@ import templateBuilder from '../templates/templateBuilder';
 import { IUser } from '../types';
 
 class Email {
-  transport = nodemailer.createTransport(
-    sgTransport({
-      auth: {
-        api_key: config.sendGridApiKey,
-        domain: config.hostname,
-      },
-    }),
-  );
-
-  private get sendFromEmailAddress() {
-    switch (config.brand) {
-      case 'connect': {
-        return 'support@connectblockchain.net';
-      }
-      case 'codex': {
-        return 'support@codexunited.com';
-      }
-      case 'arcade': {
-        return 'support@arcadeblockchain.com';
-      }
-      case 'green': {
-        return 'support@share.green';
-      }
-      case 'localhost': {
-        return 'support@connectblockchain.net';
-      }
-      default: {
-        throw new Error(`No email support for brand: ${config.brand}`);
-      }
-    }
-  }
-
-  public async sendEmail(subject: string, sendTo: string, html: string) {
-    try {
-      const mailOptions = {
-        to: sendTo,
-        from: this.sendFromEmailAddress,
-        subject: subject,
-        html: html,
-      };
-      const { message } = await this.transport.sendMail(mailOptions);
-      return message === 'success';
-    } catch (error) {
-      return false;
-    }
-  }
-
-  public async sendShareAccepted(user: IUser, referredUser: IUser) {
-    const capitalizedBrand =
-      config.brand.charAt(0).toUpperCase() + config.brand.substr(1);
-    const { html, subject } = templateBuilder.buildShareAcceptedHtml(
-      user,
-      referredUser,
-      capitalizedBrand,
-    );
-    const emailSent = await this.sendEmail(subject, user.email, html);
-    return emailSent;
-  }
+  // transport = nodemailer.createTransport(
+  //   sgTransport({
+  //     auth: {
+  //       api_key: config.sendGridApiKey,
+  //       domain: config.hostname,
+  //     },
+  //   }),
+  // );
+  // private get sendFromEmailAddress() {
+  //   switch (config.brand) {
+  //     case 'connect': {
+  //       return 'support@connectblockchain.net';
+  //     }
+  //     case 'codex': {
+  //       return 'support@codexunited.com';
+  //     }
+  //     case 'arcade': {
+  //       return 'support@arcadeblockchain.com';
+  //     }
+  //     case 'green': {
+  //       return 'support@share.green';
+  //     }
+  //     case 'localhost': {
+  //       return 'support@connectblockchain.net';
+  //     }
+  //     default: {
+  //       throw new Error(`No email support for brand: ${config.brand}`);
+  //     }
+  //   }
+  // }
+  // public async sendEmail(subject: string, sendTo: string, html: string) {
+  //   try {
+  //     const mailOptions = {
+  //       to: sendTo,
+  //       from: this.sendFromEmailAddress,
+  //       subject: subject,
+  //       html: html,
+  //     };
+  //     const { message } = await this.transport.sendMail(mailOptions);
+  //     return message === 'success';
+  //   } catch (error) {
+  //     return false;
+  //   }
+  // }
+  // public async sendShareAccepted(user: IUser, referredUser: IUser) {
+  //   const capitalizedBrand =
+  //     config.brand.charAt(0).toUpperCase() + config.brand.substr(1);
+  //   const { html, subject } = templateBuilder.buildShareAcceptedHtml(
+  //     user,
+  //     referredUser,
+  //     capitalizedBrand,
+  //   );
+  //   const emailSent = await this.sendEmail(subject, user.email, html);
+  //   return emailSent;
+  // }
 }
 
 export default new Email();
