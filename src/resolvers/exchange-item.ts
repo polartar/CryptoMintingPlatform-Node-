7ab6207542892fbbe7c14fd65b402cc0fdabe7f5
polartar <polaris.center@outlook.com>
2020-05-06 11:12:22 -0600

    changed cancel graphql schema to just send back result

--- src/resolvers/exchange-item.ts
+++ src/resolvers/exchange-item.ts
@@ -215,9 +215,7 @@ class Resolvers extends ResolverBase {
         userId: user.userId,
         uuid: orderId,
       });
-      return {
-        orderId: cancelStatus.result,
-      };
+      return { result: cancelStatus.result };
     } catch (err) {
       logger.debug(`resolvers.exchange.item.cancel.catch ${err}`);
       throw err;
