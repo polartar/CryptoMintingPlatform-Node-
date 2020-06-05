c1e8d00936c0df982991aaa313f44f0f6e898a18
polartar <polaris.center@outlook.com>
2020-06-05 11:30:48 -0400

    Adds admin query to check for api-key-service resources

--- src/resolvers/admin.ts
+++ src/resolvers/admin.ts
@@ -1,6 +1,7 @@
 import { auth, config } from '../common';
 import { Context } from '../types/context';
 import ResolverBase from '../common/Resolver-Base';
+import { credentialService } from '../services';
 
 class Resolvers extends ResolverBase {
   getImpersonationToken = async (
@@ -13,6 +14,25 @@ class Resolvers extends ResolverBase {
 
     return { token };
   };
+
+  getApiKeyServiceResource = async (
+    parent: any,
+    args: { userId: string; coin: string; resourceKey: string },
+    { user }: Context,
+  ): Promise<{ resource: string | null; error?: string }> => {
+    this.requireAdmin(user);
+    try {
+      const resource = await credentialService.get(
+        args.userId,
+        args.coin,
+        args.resourceKey,
+      );
+
+      return { resource };
+    } catch (error) {
+      return { resource: null, error: error.message };
+    }
+  };
 }
 
 const resolvers = new Resolvers();
@@ -20,5 +40,6 @@ const resolvers = new Resolvers();
 export default {
   Query: {
     getImpersonationToken: resolvers.getImpersonationToken,
+    getApiKeyServiceResource: resolvers.getApiKeyServiceResource,
   },
 };
