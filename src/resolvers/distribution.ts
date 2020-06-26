d4ca6359785611467508e2fbf1db62112f9bf704
polartar <polaris.center@outlook.com>
2020-06-26 10:37:41 -0600

    adds a query to look up distribution data for a user

---
+++ src/resolvers/distribution.ts
@@ -0,0 +1,39 @@
+import { startOfDay, endOfDay } from 'date-fns';
+import { Context } from '../types/context';
+import ResolverBase from '../common/Resolver-Base';
+import User from '../models/user';
+import { getPipeline } from '../pipelines/get_distribution_data';
+
+class Resolvers extends ResolverBase {
+  getDistributionDataByEmail = async (
+    parent: any,
+    { email, date }: { email: string; date: Date },
+    { user }: Context,
+  ) => {
+    this.requireAdmin(user);
+
+    const startOfDate = startOfDay(date);
+    const endOfDate = endOfDay(date);
+    const pipeline = getPipeline(email, startOfDate, endOfDate);
+
+    const [data] = await User.aggregate(pipeline);
+
+    return data as Array<{
+      firstName: string;
+      lastName: string;
+      email: string;
+      userId: string;
+      totalPoolPoints: number;
+      distributionAmount: number;
+      points: Array<{ pointType: string; amount: number }>;
+    }>;
+  };
+}
+
+const resolvers = new Resolvers();
+
+export default {
+  Query: {
+    distributionData: resolvers.getDistributionDataByEmail,
+  },
+};
