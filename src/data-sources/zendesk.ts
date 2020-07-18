63c568498663cc4e5f588041df355f89dc692232
polartar <polaris.center@outlook.com>
2020-07-18 11:56:30 -0600

    Temporarily save zendesk tickets to db

--- src/data-sources/zendesk.ts
+++ src/data-sources/zendesk.ts
@@ -1,6 +1,7 @@
 import { RESTDataSource, RequestOptions } from 'apollo-datasource-rest';
 import { config, logger } from '../common';
 import { IZendeskRequest, IZendeskTicket } from '../types';
+import { SupportTicket } from '../models/support-ticket';
 
 class Zendesk extends RESTDataSource {
   baseURL = 'https://robotsupport.zendesk.com/api/v2';
@@ -34,17 +35,28 @@ class Zendesk extends RESTDataSource {
     logger.debug(
       `data-sources.zendesk.createTicket.ticket.userId: ${ticket.userId}`,
     );
-    const { audit } = await this.post('/tickets.json', {
-      ticket: {
-        subject: ticket.subject,
-        comment: { body: ticket.comment },
-        requester: ticket.requester,
-        external_id: ticket.userId,
-        additional_tags: ['wallet'],
-        brand_id: this.brandId,
-      },
+    // const { audit } = await this.post('/tickets.json', {
+    //   ticket: {
+    //     subject: ticket.subject,
+    //     comment: { body: ticket.comment },
+    //     requester: ticket.requester,
+    //     external_id: ticket.userId,
+    //     additional_tags: ['wallet'],
+    //     brand_id: this.brandId,
+    //   },
+    // });
+    // return audit.ticket_id;
+    const createdTicket = await SupportTicket.create({
+      subject: ticket.subject,
+      comment: ticket.comment,
+      requester: ticket.requester,
+      externalId: ticket.userId,
+      additionalTags: ['wallet'],
+      brandId: this.brandId,
+      status: 'pending',
     });
-    return audit.ticket_id;
+
+    return createdTicket._id.toString();
   }
 
   public async getUserTickets(
@@ -52,28 +64,41 @@ class Zendesk extends RESTDataSource {
     page: number,
   ): Promise<IZendeskTicket[]> {
     logger.debug(`data-sources.zendesk.getUserTickets: ${userId}`);
-    const { results } = (await this.get('/search.json', {
-      query: `type:ticket external_id:${userId}`,
-      page: page,
-    })) as { results: any[] };
-    return results.map((result: any) => {
-      const {
-        created_at: createdAt,
-        updated_at: updatedAt,
-        status,
-        raw_subject: subject,
-        id,
-        description,
-      } = result;
-      return {
-        createdAt: new Date(createdAt),
-        updatedAt: new Date(updatedAt),
-        status,
-        subject,
-        id,
-        description,
-      };
-    });
+    // const { results } = (await this.get('/search.json', {
+    //   query: `type:ticket external_id:${userId}`,
+    //   page: page,
+    // })) as { results: any[] };
+    // return results.map((result: any) => {
+    //   const {
+    //     created_at: createdAt,
+    //     updated_at: updatedAt,
+    //     status,
+    //     raw_subject: subject,
+    //     id,
+    //     description,
+    //   } = result;
+    //   return {
+    //     createdAt: new Date(createdAt),
+    //     updatedAt: new Date(updatedAt),
+    //     status,
+    //     subject,
+    //     id,
+    //     description,
+    //   };
+    // });
+    const tickets = await SupportTicket.find({ externalId: userId })
+      .sort('-created')
+      .skip(page * 10)
+      .limit(10);
+
+    return tickets.map(ticket => ({
+      createdAt: ticket.created,
+      updatedAt: ticket.updated,
+      status: ticket.status,
+      subject: ticket.subject,
+      id: ticket._id.toString(),
+      description: ticket.comment,
+    }));
   }
 }
 
