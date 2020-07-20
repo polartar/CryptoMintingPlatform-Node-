bc5adc97d89d83b21c9694179899c287b3cf2a9a
polartar <polaris.center@outlook.com>
2020-07-20 10:55:38 -0600

    fix: replaces zendesk URL

--- src/data-sources/zendesk.ts
+++ src/data-sources/zendesk.ts
@@ -1,10 +1,9 @@
 import { RESTDataSource, RequestOptions } from 'apollo-datasource-rest';
 import { config, logger } from '../common';
 import { IZendeskRequest, IZendeskTicket } from '../types';
-import { SupportTicket } from '../models/support-ticket';
 
 class Zendesk extends RESTDataSource {
-  baseURL = 'https://robotsupport.zendesk.com/api/v2';
+  baseURL = config.zendeskUrl;
   brandId = this.getBrandId(config.brand);
 
   willSendRequest(request: RequestOptions) {
@@ -25,6 +24,9 @@ class Zendesk extends RESTDataSource {
       case 'green': {
         return 360001823714;
       }
+      case 'blue': {
+        return 360001738613;
+      }
       default: {
         return 360001823734;
       }
@@ -35,28 +37,17 @@ class Zendesk extends RESTDataSource {
     logger.debug(
       `data-sources.zendesk.createTicket.ticket.userId: ${ticket.userId}`,
     );
-    // const { audit } = await this.post('/tickets.json', {
-    //   ticket: {
-    //     subject: ticket.subject,
-    //     comment: { body: ticket.comment },
-    //     requester: ticket.requester,
-    //     external_id: ticket.userId,
-    //     additional_tags: ['wallet'],
-    //     brand_id: this.brandId,
-    //   },
-    // });
-    // return audit.ticket_id;
-    const createdTicket = await SupportTicket.create({
-      subject: ticket.subject,
-      comment: ticket.comment,
-      requester: `${ticket.requester.email}: ${ticket.requester.name}`,
-      externalId: ticket.userId,
-      additionalTags: ['wallet'],
-      brandId: this.brandId,
-      status: 'pending',
+    const { audit } = await this.post('/tickets.json', {
+      ticket: {
+        subject: ticket.subject,
+        comment: { body: ticket.comment },
+        requester: ticket.requester,
+        external_id: ticket.userId,
+        additional_tags: ['wallet'],
+        brand_id: this.brandId,
+      },
     });
-
-    return createdTicket._id.toString();
+    return audit.ticket_id;
   }
 
   public async getUserTickets(
@@ -64,41 +55,28 @@ class Zendesk extends RESTDataSource {
     page: number,
   ): Promise<IZendeskTicket[]> {
     logger.debug(`data-sources.zendesk.getUserTickets: ${userId}`);
-    // const { results } = (await this.get('/search.json', {
-    //   query: `type:ticket external_id:${userId}`,
-    //   page: page,
-    // })) as { results: any[] };
-    // return results.map((result: any) => {
-    //   const {
-    //     created_at: createdAt,
-    //     updated_at: updatedAt,
-    //     status,
-    //     raw_subject: subject,
-    //     id,
-    //     description,
-    //   } = result;
-    //   return {
-    //     createdAt: new Date(createdAt),
-    //     updatedAt: new Date(updatedAt),
-    //     status,
-    //     subject,
-    //     id,
-    //     description,
-    //   };
-    // });
-    const tickets = await SupportTicket.find({ externalId: userId })
-      .sort('-created')
-      .skip(page * 10)
-      .limit(10);
-
-    return tickets.map(ticket => ({
-      createdAt: ticket.created,
-      updatedAt: ticket.updated,
-      status: ticket.status,
-      subject: ticket.subject,
-      id: ticket._id.toString(),
-      description: ticket.comment,
-    }));
+    const { results } = (await this.get('/search.json', {
+      query: `type:ticket external_id:${userId}`,
+      page: page,
+    })) as { results: any[] };
+    return results.map((result: any) => {
+      const {
+        created_at: createdAt,
+        updated_at: updatedAt,
+        status,
+        raw_subject: subject,
+        id,
+        description,
+      } = result;
+      return {
+        createdAt: new Date(createdAt),
+        updatedAt: new Date(updatedAt),
+        status,
+        subject,
+        id,
+        description,
+      };
+    });
   }
 }
 
