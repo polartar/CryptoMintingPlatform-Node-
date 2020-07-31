d3f35d96e16ca58a208a1fb1e7a8e391cc432003
polartar <polaris.center@outlook.com>
2020-07-31 13:15:09 -0600

    undo testing changes

--- src/services/gala-gaming-api.ts
+++ src/services/gala-gaming-api.ts
@@ -4,15 +4,13 @@ import { ServerToServerService } from './server-to-server';
 class GalaGamingApiService extends ServerToServerService {
   baseUrl = `${config.galaGamingApiUrl}/api`;
 
-  public getGameJWT = (userId: string) => userId;
+  public getGameJWT = async (userId: string) => {
+    const jwtAxios = this.getAxios({ role: 'system' });
 
-  // public getGameJWT = async (userId: string) => {
-  // const jwtAxios = this.getAxios({ role: 'system' });
+    const { data } = await jwtAxios.get(`${this.baseUrl}/token/${userId}`);
 
-  // const { data } = await jwtAxios.get(`${this.baseUrl}/token/${userId}`);
-
-  //   return data.token;
-  // };
+    return data.token;
+  };
 }
 
 export default new GalaGamingApiService();
