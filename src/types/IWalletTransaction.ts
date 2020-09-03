6e13d338477fdb907240069c69e148ec7951bc28
polartar <polaris.center@outlook.com>
2020-09-03 13:01:02 -0400

    Wallet transaction schema change

--- src/types/IWalletTransaction.ts
+++ src/types/IWalletTransaction.ts
@@ -1,9 +1,39 @@
-export interface IWalletTransaction {
-  type: string; //
+export interface AssignedNode {
+  hardwareId: string;
+  licenseId: string;
+  ethAddress: string;
+  userId: string;
+}
+
+export interface SpecificTransactionProperties {
+  returnedEth?: string;
+  returnedEthHex?: string;
+  returnedEthDecimals?: number;
+  returnedEthDecimalsStored?: number;
+  baseId?: string;
+  tokenId?: string;
+  logIndex?: number | null;
+  nft?: boolean;
+  contractName?: string;
+  contractMethod?: string;
+  mintTransaction?: boolean;
+  assignedNode?: AssignedNode;
+}
+
+export enum TransactionType {
+  Eth = 'ETH',
+  Erc1155 = 'ERC-1155',
+  Erc20 = 'ERC-20',
+  ExternalContract = 'ExternalContract',
+}
+
+export interface IWalletTransaction extends SpecificTransactionProperties {
+  type: TransactionType;
   status: string;
   timestamp: number;
   to: string;
   from: string;
+  operator: string;
   amount: number | string;
   fullHexAmount: string;
   decimalsStored: number;
@@ -15,21 +45,4 @@ export interface IWalletTransaction {
   gasPriceDecimals: number;
   hash: string;
   nonce: number;
-  toUser: string;
-  fromUser: string;
-  baseId?: string;
-  tokenId?: string;
-  logIndex?: number | null;
-  batchIndex?: number | null;
-  batchCount?: number;
-  nft?: boolean;
-  contractMethod?: string;
-  contractType?: string;
-  mintTransaction?: boolean;
-  assignedNode?: {
-    hardwareId: string;
-    licenseId: string;
-    ethAddress: string;
-    userId: string;
-  };
 }
