847a226c9408b9038d5f57a71976afa2c9d6ade3
polartar <polaris.center@outlook.com>
2020-09-20 15:47:02 -0600

    logs

--- src/resolvers/share-gala.ts
+++ src/resolvers/share-gala.ts
@@ -12,7 +12,7 @@ import { logResolver } from '../common/logger';
 export class GalaShareResolver extends ShareResolver {
   private getRewardTotals = async (userId: string) => {
     const [result] = await User.aggregate(referralRewardsPipeline(userId));
-
+    console.log('getRewardTotals::1:result', result);
     return (
       result || {
         btcEarned: 0,
@@ -34,7 +34,9 @@ export class GalaShareResolver extends ShareResolver {
     // this.requireBrand().toBeIn(['arcade', 'gala']);
     try {
       const { brand } = config;
+      console.log('galaShareConfig::1:brand', brand);
       const dbUser = await user.findFromDb();
+      console.log('galaShareConfig::2:user', dbUser.id);
 
       const {
         available,
@@ -42,7 +44,15 @@ export class GalaShareResolver extends ShareResolver {
         earnedShares,
         numberOfActivations,
       } = await this.getShareConfigs(dbUser);
+      console.log('galaShareConfig::3:available', available);
+      console.log('galaShareConfig::4:unactivated', unactivated);
+      console.log('galaShareConfig::5:earnedShares', earnedShares);
+      console.log(
+        'galaShareConfig::6:numberOfActivations',
+        numberOfActivations,
+      );
       const btcUsdPrice = await cryptoFavorites.getBtcUsdPrice();
+      console.log('galaShareConfig::7:btcUsdPrice', btcUsdPrice);
       logger.JSON.debug({
         available,
         unactivated,
@@ -50,13 +60,20 @@ export class GalaShareResolver extends ShareResolver {
         numberOfActivations,
       });
       const userWallet = dbUser?.wallet;
+      console.log(
+        'galaShareConfig::8:userWallet.shares',
+        userWallet && userWallet.shares,
+      );
       if (!userWallet) throw new Error('User wallet not initialized');
       const { confirmed, unconfirmed } = await wallet
         .coin('btc')
         .getBalance(user.userId);
+      console.log('galaShareConfig::9:confirmed', confirmed);
+      console.log('galaShareConfig::10:unconfirmed', unconfirmed);
       logger.JSON.debug({ confirmed, unconfirmed });
       const activatedShares =
         (userWallet && userWallet.shares && userWallet.shares[brand]) || 0;
+      console.log('galaShareConfig::11:ac', unconfirmed);
       logger.obj.debug({ activatedShares });
       const [
         {
@@ -69,6 +86,19 @@ export class GalaShareResolver extends ShareResolver {
           basicWalletBenefits,
         },
       ] = available;
+      console.log('galaShareConfig::12:referrerReward', referrerReward);
+      console.log('galaShareConfig::13:companyFee', companyFee);
+      console.log('galaShareConfig::14:rewardCurrency', rewardCurrency);
+      console.log('galaShareConfig::15:rewardAmount', rewardAmount);
+      console.log(
+        'galaShareConfig::16:userBalanceThreshold',
+        userBalanceThreshold,
+      );
+      console.log('galaShareConfig::17:upgradeBenefits', upgradeBenefits);
+      console.log(
+        'galaShareConfig::18:basicWalletBenefits',
+        basicWalletBenefits,
+      );
       const {
         btcEarned,
         friendsJoined,
@@ -78,6 +108,16 @@ export class GalaShareResolver extends ShareResolver {
         nodesOwned,
         upgradedReferrals,
       } = await this.getRewardTotals(dbUser.id);
+      console.log('galaShareConfig::19:btcEarned', btcEarned);
+      console.log('galaShareConfig::20:friendsJoined', friendsJoined);
+      console.log('galaShareConfig::21:friendsPlayed', friendsPlayed);
+      console.log('galaShareConfig::22:galaEarned', galaEarned);
+      console.log(
+        'galaShareConfig::23:nodesPurchasedByReferrals',
+        nodesPurchasedByReferrals,
+      );
+      console.log('galaShareConfig::24:nodesOwned', nodesOwned);
+      console.log('galaShareConfig::25:upgradedReferrals', upgradedReferrals);
       return {
         userWallet, // required for sub-resolver
         goldMember: !!userWallet?.activations?.gala?.activated,
