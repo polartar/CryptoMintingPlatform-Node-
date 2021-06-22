e14977aac42dc88e7b312157961de399a60cc0da
polartar <polaris.center@outlook.com>
2021-06-22 21:05:35 +0000

    Vault mock

--- src/types/IVault.ts
+++ src/types/IVault.ts
@@ -26,3 +26,32 @@ export interface IVaultGasFee {
   expires: Date;
   name: string;
 }
+
+export enum StatusResponse {
+  Success,
+  Error
+}
+
+export enum ErrorResponseCode {
+  InvalidEncryptionPassword,
+  BlockchainError,
+  InternalError,
+}
+
+export interface IErrorResponse {
+  message: String
+  code: ErrorResponseCode,
+  stack?: string,
+}
+
+export interface IVaultRetrieveResponseData {
+  symbol: string,
+  amount: number,
+  transactionId: string,
+}
+
+export interface IVaultRetrieveResponse {
+  data?: IVaultRetrieveResponseData,
+  error?: IErrorResponse,
+  status: StatusResponse,
+}
