d75ae7bb9623f2c83674c37f9530f7f28113d2ad
polartar <polaris.center@outlook.com>
2021-08-02 16:28:35 -0600

    fixed the BigNumber problem

--- src/services/token-generator/token-minter-erc20.ts
+++ src/services/token-generator/token-minter-erc20.ts
@@ -4,67 +4,63 @@ import * as abi from '../../common/ABI/erc20-green.json';
 import { config } from '../../common';
 //import { UserWithTokenAmount } from '../types/user';
 import { ContractData } from '../../types/eth-contracts/contract';
-import BigNumber from 'bignumber.js';
 //import { Erc20 as Erc20Contract } from '../../types/eth-contracts/erc20-green';
 //import nodeSelector from './node-selector';
 
 class TokenMinter {
-    private provider = new ethers.providers.JsonRpcProvider(
-        config.ethNodeUrl,
+  private provider = new ethers.providers.JsonRpcProvider(config.ethNodeUrl);
+
+  private signer: ethers.Wallet;
+  private contract: ethers.Contract;
+  private decimalPlaces: number;
+
+  constructor(contractData: ContractData) {
+    this.signer = new ethers.Wallet(contractData.privateKey, this.provider);
+    this.contract = new ethers.Contract(contractData.address, abi, this.signer);
+    this.decimalPlaces = contractData.decimalPlaces;
+  }
+
+  public mintToGetFromVault = async (toMint: IMintDestination) => {
+    const [nonce, gasPrice] = await Promise.all([
+      this.signer.getTransactionCount(),
+      this.provider.getGasPrice(),
+    ]);
+
+    const uint256Amt: number = +toMint.amountDecimal * Math.pow(10, 8); //TODO : pull decimalPlaces from this.decimalPlaces
+
+    const destinationAddresses: string[] = [toMint.destinationAddress];
+    const destinationAmount: number[] = [uint256Amt];
+
+    //let tx = await this.contract.distributeMinting(destinationAddresses, destinationAmount);
+    // const parameterToDistribute = {
+    //     distAddresses: destinationAddresses,
+    //     distValues: destinationAmount
+    // };
+
+    const contractMethod = this.contract.interface.encodeFunctionData(
+      'distributeMinting',
+      [destinationAddresses, destinationAmount],
     );
 
-    private signer: ethers.Wallet;
-    private contract: ethers.Contract;
-    private decimalPlaces: number;
+    const transaction = await this.signer.signTransaction({
+      to: this.contract.address,
+      data: contractMethod,
+      gasLimit: 1000000,
+      value: '0x0',
+      nonce: nonce,
+      gasPrice: Math.floor(gasPrice.toNumber() * 1.1),
+    });
 
-    constructor(contractData: ContractData) {
-        this.signer = new ethers.Wallet(contractData.privateKey, this.provider);
-        this.contract = new ethers.Contract(
-            contractData.address,
-            abi,
-            this.signer,
-        );
-    }
+    //TODO : when we do nodeSelector for the mint, put it in here.
+    //const parsedTransaction = utils.parseTransaction(transaction);
+    //await nodeSelector.getNodeToMineTransaction(parsedTransaction.hash);
 
-    public mintToGetFromVault = async (toMint: IMintDestination) => {
-        const [nonce, gasPrice] = await Promise.all([
-            this.signer.getTransactionCount(),
-            this.provider.getGasPrice(),
-        ]);
-
-        const amountConverted:BigNumber = new BigNumber(+toMint.amountDecimal * +Math.pow(10, this.decimalPlaces));
-
-        const destinationAddresses: string[] = [toMint.destinationAddress];
-        const destinationAmount: BigNumber[] = [amountConverted];
-
-        //let tx = await this.contract.distributeMinting(destinationAddresses, destinationAmount);
-        // const parameterToDistribute = {
-        //     distAddresses: destinationAddresses,
-        //     distValues: destinationAmount
-        // };
-
-        const contractMethod = this.contract.interface.encodeFunctionData("distributeMinting", [destinationAddresses, destinationAmount]);
-
-        const transaction = await this.signer.signTransaction({
-            to: this.contract.address,
-            data: contractMethod,
-            gasLimit: 1000000,
-            value: '0x0',
-            nonce: nonce,
-            gasPrice: Math.floor(gasPrice.toNumber() * 1.1),
-            
-        });
-
-        //TODO : when we do nodeSelector for the mint, put it in here.
-        //const parsedTransaction = utils.parseTransaction(transaction);
-        //await nodeSelector.getNodeToMineTransaction(parsedTransaction.hash);
-
-        return this.provider.sendTransaction(transaction);
-    };
+    return this.provider.sendTransaction(transaction);
+  };
 }
-export interface IMintDestination{
-    destinationAddress: string;
-    amountDecimal: number;
+export interface IMintDestination {
+  destinationAddress: string;
+  amountDecimal: number;
 }
 
 export default TokenMinter;
