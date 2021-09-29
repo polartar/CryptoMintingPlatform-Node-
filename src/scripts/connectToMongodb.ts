73008175ac4ec02cb3c8cc39f6ceaca45c6d4f75
polartar <polaris.center@outlook.com>
2021-09-29 10:42:55 -0500

    Fixed Promise errors

--- src/scripts/connectToMongodb.ts
+++ src/scripts/connectToMongodb.ts
@@ -2,7 +2,7 @@ import * as mongoose from 'mongoose';
 import { MongoError } from 'mongodb';
 
 export default function connectToMongoose(mongodbUri: string) {
-  return new Promise((resolve, reject) => {
+  return new Promise<void>((resolve, reject) => {
     mongoose.connect(mongodbUri, (err: MongoError) => {
       if (err) {
         reject(err);
