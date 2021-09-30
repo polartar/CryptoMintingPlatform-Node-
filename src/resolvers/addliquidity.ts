46b57a0b58da9dd076eb1293137b03b0330c5f13
polartar <polaris.center@outlook.com>
2021-09-30 11:01:50 -0500

    liquidity

--- src/resolvers/addliquidity.ts
+++ src/resolvers/addliquidity.ts
@@ -14,8 +14,10 @@ import {
   //CurrencyAmount
 } from '@uniswap/sdk-core';
 import { abi as IUniswapV3PoolABI } from '@uniswap/v3-core/artifacts/contracts/interfaces/IUniswapV3Pool.sol/IUniswapV3Pool.json';
+import { abi as IUniswapV3FactoryABI } from '@uniswap/v3-core/artifacts/contracts/UniswapV3Factory.sol/UniswapV3Factory.json';
 import { UniswapPair } from 'simple-uniswap-sdk';
 import EthWallet from '../wallet-api/coin-wallets/eth-wallet';
+import e from 'express';
 
 const { chainId, ethNodeUrl } = config;
 
@@ -24,10 +26,12 @@ class LiquidityResolver extends ResolverBase {
     parent: any,
     args: {
       token0: string;
+      decimalPlaces0: number;
       token1: string;
+      decimalPlaces1: number;
       fee: number;
+      amountToken0: string;
       amountToken1: string;
-      amountToken2: string;
       walletPassword: string;
       initialPrice: string;
     },
@@ -37,95 +41,6 @@ class LiquidityResolver extends ResolverBase {
     const walletApi = wallet.coin('ETH') as EthWallet;
     const { receiveAddress } = await walletApi.getWalletInfo(user);
     try {
-      const uniswapPair = new UniswapPair({
-        fromTokenContractAddress: args.token0,
-        toTokenContractAddress: args.token1,
-        ethereumAddress: receiveAddress,
-        chainId: chainId,
-      });
-
-      const uniswapPairFactory = await uniswapPair.createFactory();
-      const trade = await uniswapPairFactory.trade('1');
-
-      const provider = new ethers.providers.JsonRpcProvider(ethNodeUrl);
-
-      const tokenFrom = trade.fromToken;
-      const tokenTo = trade.toToken;
-
-      const token0 = new Token(
-        chainId,
-        tokenFrom.contractAddress,
-        tokenFrom.decimals,
-      );
-
-      const token1 = new Token(
-        chainId,
-        tokenTo.contractAddress,
-        tokenTo.decimals,
-      );
-
-      const poolAddress = Pool.getAddress(token0, token1, args.fee);
-
-      const poolContract = new ethers.Contract(
-        poolAddress,
-        IUniswapV3PoolABI,
-        provider,
-      );
-
-      // const immutables: Immutables = {
-      //   factory: await poolContract.factory(),
-      //   token0: await poolContract.token0(),
-      //   token1: await poolContract.token1(),
-      //   fee: await poolContract.fee(),
-      //   tickSpacing: await poolContract.tickSpacing(),
-      //   maxLiquidityPerTick: await poolContract.maxLiquidityPerTick(),
-      // };
-
-      // const slot = await poolContract.slot0();
-      // const PoolState: State = {
-      //   liquidity: await poolContract.liquidity(),
-      //   sqrtPriceX96: slot[0],
-      //   tick: slot[1],
-      //   observationIndex: slot[2],
-      //   observationCardinality: slot[3],
-      //   observationCardinalityNext: slot[4],
-      //   feeProtocol: slot[5],
-      //   unlocked: slot[6],
-      // };
-
-      const block = await provider.getBlock(provider.getBlockNumber());
-
-      // const NEW_POOL = new Pool(
-      //   token0,
-      //   token1,
-      //   immutables.fee,
-      //   PoolState.sqrtPriceX96.toString(),
-      //   PoolState.liquidity.toString(),
-      //   PoolState.tick,
-      // );
-
-      // const liquidityNumber = Number(PoolState.liquidity);
-      // const portionLiqudity = liquidityNumber * 0.0002;
-
-      // const price0 = NEW_POOL.priceOf(token0)
-      //   .toFixed(token0.decimals, NEW_POOL.priceOf(token0).scalar)
-      //   .toString();
-      // const price1 = NEW_POOL.priceOf(token1)
-      //   .toFixed(token1.decimals, NEW_POOL.priceOf(token1).scalar)
-      //   .toString();
-
-      // const position = new Position({
-      //   pool: NEW_POOL,
-      //   liquidity: liquidityNumber.toString(),
-      //   tickLower:
-      //     nearestUsableTick(PoolState.tick, immutables.tickSpacing) -
-      //     immutables.tickSpacing * 2,
-      //   tickUpper:
-      //     nearestUsableTick(PoolState.tick, immutables.tickSpacing) +
-      //     immutables.tickSpacing * 2,
-      // });
-      const deadline = block.timestamp + 200;
-
       let passwordDecripted;
       try {
         const encryptedKey = await walletApi.getEncryptedPrivKey(user.userId);
@@ -139,45 +54,141 @@ class LiquidityResolver extends ResolverBase {
         logger.warn('EncryptedKey no return instead we reach a 401 status' + e);
       }
 
+      const provider = new ethers.providers.JsonRpcProvider(ethNodeUrl);
+      const gasPrice = provider.getGasPrice();
+
+      const token0 = new Token(chainId, args.token0, args.decimalPlaces0);
+
+      const token1 = new Token(chainId, args.token1, args.decimalPlaces1);
+
       const ethersWallet = new ethers.Wallet(passwordDecripted);
       const account = ethersWallet.connect(provider);
 
-      const tokenA = token0.address;
-      // let abi = ["function approve(address _spender, uint256 _value) public returns (bool success)"]
-      // let contract = new ethers.Contract(tokenAddress, abi, provider)
-      // await contract.approve(accountAddress, amount)
-
-      const tokenB = token1.address;
-      // let abi = ["function approve(address _spender, uint256 _value) public returns (bool success)"]
-      // let contract = new ethers.Contract(tokenAddress, abi, provider)
-      // await contract.approve(accountAddress, amount)
-
-      const amountADesired = args.amountToken1;
-      const amountBDesired = args.amountToken2;
-      const amountAMin = 0;
-      const amountBMin = 0;
-      const to = receiveAddress;
-
-      const addLiquidity = new ethers.Contract(
-        '0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D',
-        [
-          'function addLiquidity(  address tokenA,  address tokenB,  uint amountADesired,  uint amountBDesired,  uint amountAMin,  uint amountBMin,  address to,  uint deadline) external returns (uint amountA, uint amountB, uint liquidity)',
-        ],
+      const factoryContract = new ethers.Contract(
+        '0x1F98431c8aD98523631AE4a59f267346ea31F984',
+        IUniswapV3FactoryABI,
         account,
       );
 
-      const tx = await addLiquidity.addLiquidity(
-        tokenA,
-        tokenB,
-        amountADesired,
-        amountBDesired,
-        amountAMin,
-        amountBMin,
-        to,
-        deadline,
+      let txPool;
+      try {
+        const createPool = await factoryContract.createPool(
+          token0.address,
+          token1.address,
+          500,
+          { gasPrice: gasPrice, gasLimit: 1000000 },
+        );
+        txPool = await createPool.wait();
+      } catch (error) {
+        logger.warn(
+          'FactoryContract error create Pool address' + error.message,
+        );
+      }
+
+      let poolAddress;
+      if (txPool === undefined) {
+        try {
+          poolAddress = await factoryContract.getPool(
+            token0.address,
+            token1.address,
+            500,
+          );
+        } catch (error) {
+          logger.warn('FactoryContract get pool error' + error.message);
+        }
+      }
+
+      const poolContract = new ethers.Contract(
+        poolAddress,
+        IUniswapV3PoolABI,
+        account,
       );
 
-      const receipt = await tx.wait();
+      const slot = await poolContract.slot0();
+      const PoolState: State = {
+        liquidity: await poolContract.liquidity(),
+        sqrtPriceX96: slot[0],
+        tick: slot[1],
+        observationIndex: slot[2],
+        observationCardinality: slot[3],
+        observationCardinalityNext: slot[4],
+        feeProtocol: slot[5],
+        unlocked: slot[6],
+      };
+
+      const liquidity = await poolContract.liquidity();
+      const priceInitializer = ethers.BigNumber.from(1);
+
+      let tx;
+      let receipt;
+      try {
+        tx = await poolContract.initialize(priceInitializer, {
+          gasPrice: gasPrice,
+          gasLimit: 1000000,
+        });
+        receipt = await tx.wait();
+      } catch (e) {
+        logger.warn(
+          'The pool already exists, the fee is invalid, or the token arguments are invalid.',
+        );
+      }
+
+      const immutables: Immutables = {
+        factory: await poolContract.factory(),
+        token0: await poolContract.token0(),
+        token1: await poolContract.token1(),
+        fee: await poolContract.fee(),
+        tickSpacing: await poolContract.tickSpacing(),
+        maxLiquidityPerTick: await poolContract.maxLiquidityPerTick(),
+      };
+
+      let txMint;
+      let receiptMint;
+      try {
+        txMint = await poolContract.mint(
+          receiveAddress,
+          nearestUsableTick(PoolState.tick, immutables.tickSpacing) -
+            immutables.tickSpacing * 2,
+          nearestUsableTick(PoolState.tick, immutables.tickSpacing) +
+            immutables.tickSpacing * 2,
+          liquidity.mul(2).div(1000),
+          { gasPrice: gasPrice, gasLimit: 1000000 },
+        );
+        receiptMint = await txMint.wait();
+      } catch (error) {
+        logger.warn("can't mint position" + error.message);
+      }
+
+      const block = await provider.getBlock(provider.getBlockNumber());
+      const deadline = block.timestamp + 200;
+
+      //create a pool
+      const NEW_POOL = new Pool(
+        token0,
+        token1,
+        immutables.fee,
+        PoolState.sqrtPriceX96.toString(),
+        PoolState.liquidity.toString(),
+        PoolState.tick,
+      );
+
+      // const position = new Position({
+      //   pool: NEW_POOL,
+      //   liquidity: PoolState.liquidity.mul(2).div(1000).toString(),
+      //   tickLower: nearestUsableTick(PoolState.tick, immutables.tickSpacing) - immutables.tickSpacing * 2,
+      //   tickUpper: nearestUsableTick(PoolState.tick, immutables.tickSpacing) + immutables.tickSpacing * 2
+      // })
+
+      const exampleType = 0;
+      // Example 0: Setting up calldata for minting a Position
+      // if (exampleType == 0) {
+      //   const all = NonfungiblePositionManager.addCallParameters(position, {
+      //     slippageTolerance: new Percent(50, 10_000),
+      //     recipient: receiveAddress,
+      //     deadline: deadline
+      //   });
+      //   console.log(all)
+      // }
 
       return {
         poolAddress: poolAddress,
@@ -195,8 +206,8 @@ class LiquidityResolver extends ResolverBase {
       startingPrice: string;
       minPrice: string;
       maxPrice: string;
+      amountToken0: string;
       amountToken1: string;
-      amountToken2: string;
       walletPassword: string;
     },
     { user, wallet }: Context,
